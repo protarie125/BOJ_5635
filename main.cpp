6defa1c88a740a9eff2d411024d6b97d6fb65ae3
protarie125 <protarie@gmail.com>
2024-01-28 15:38:54 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+using T = tuple<ll, ll, ll, string>;
+using vT = vector<T>;
+
+ll n;
+vT lst;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  cin >> n;
+
+  lst = vT(n);
+  for (auto&& [y, m, d, s] : lst) {
+    cin >> s >> d >> m >> y;
+  }
+
+  sort(lst.begin(), lst.end(), greater());
+
+  cout << get<3>(lst.front()) << '\n' << get<3>(lst.back());
+
+  return 0;
+}
